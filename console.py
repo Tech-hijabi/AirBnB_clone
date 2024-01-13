cc519bf9f2cd9f7195c18cb0365c3d616bd76ca7
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-01-13 20:23:50 +0100

    fourth set

--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
 from shlex import split
 import cmd
 import re
@@ -100,14 +101,67 @@ class HBNBCommand(cmd.Cmd):
 
         else:
             obj_list = []
-            combined_values = storage.all().values()
-            for obj in combined_values:
+            zipped_values = storage.all().values()
+            for obj in zipped_values:
                 if len(line_arg) > 0 and line_arg[0] == obj.__class__.__name__:
                     obj_list.append(obj.__str__())
                 elif len(line_arg) == 0:
                     obj_list.append(obj.__str__())
                     
             print(obj_list)
+            
+    def do_update(self, arg):
+        line_arg = parse(arg)
+        obj_dict = storage.all()
+
+        if len(line_arg) == 0:
+            print("** class name missing **")
+            return False
+
+        elif line_arg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return False
+
+            
+        elif len(line_arg) == 1:
+            print("** instance id missing **")
+            return False
+
+        elif "{}.{}".format(line_arg[0], line_arg[1]) not in obj_dict:
+            print("** no instance found **")
+
+
+        elif len(line_arg) == 2:
+            print("** attribute name missing **")
+            return False
+
+        
+        elif len(line_arg) == 3:
+            try:
+                type(eval(line_arg[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+
+        
+        if len(line_arg) == 4:
+            obj = obj_dict["{}.{}".format(line_arg[0], line_arg[1])]
+            
+            if line_arg[2] in obj.__class__.__dict__.__keys():
+                value_type = type(obj.__class__.__dict__[line_arg[2]])
+                obj.__dict__[line_arg[2]] =  value_type(line_arg[3])
+                
+        elif type(eval(line_arg[2])) == dict:
+            obj = obj_dict["{}.{}".format(line_arg[0], line_arg[1])]
+            for k, v in eval(line_arg[2]).items():
+                if (k in obj.__class__.__dict__.keys() and 
+                    type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
+
 
     def emptyline(self):
         """Called when an empty line is entered in response to the prompt."""
